2617f0b61bd45aea7bc7ac1e795be1f86db64c59
Jeff Moorhead <jmoorhead@performline.com>
2022-12-22 15:45:02 -0500

    add HTML template

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 )
 
+type url struct {
+	Root string `form:"root"`
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 
+	t := &Template{
+		templates: template.Must(template.ParseGlob("static/*.html")),
+	}
+
 	e := echo.New()
-	e.GET("/home", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "<p>Hello world!</p>")
+	e.Renderer = t
+	e.GET("/", func(c echo.Context) error {
+		log.Print("incoming request!")
+		err := c.Render(http.StatusOK, "index", nil)
+		if err != nil {
+			log.Printf("an unknown error occurred: %v", err)
+			return err
+		}
+
+		return nil
+	})
+
+	e.POST("/add", func(c echo.Context) error {
+		var u url
+		err := c.Bind(&u)
+		if err != nil {
+			return c.HTML(http.StatusBadRequest, "<p>could not process url</p>")
+		}
+
+		return c.HTML(http.StatusOK, fmt.Sprintf("<p>You submitted %v as the url to shorten</p>", u.Root))
 	})
 
 	log.Fatal(e.Start(":8080"))
